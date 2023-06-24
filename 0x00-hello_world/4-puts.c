3c5f901c4f05cf770ac521e9ea0b6051063e0323
hameenai <hameenabdulrahaman99@gmail.com>
2023-06-24 13:41:57 -0700

    All files on c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - this is below
+ *return - description here
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
