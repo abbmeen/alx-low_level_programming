40069380718df9e11027a264e3e1012602cf35c0
hameenai <hameenabdulrahaman99@gmail.com>
2023-07-04 07:04:09 -0700

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - it is main
+ *Declaration - jt us declaration
+ *Return: always 0
+ */
+
+int main(void)
+{
+	char str[9] = "_putchar";
+
+	for (int i = 0; i = 9; i++)
+	{
+		putchar(str[i]);
+	}
+	return (0);
+
+}
